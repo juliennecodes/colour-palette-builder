0222a3ccd952ba878415a9248d7a556156a582e1
Julienne San Luis <julienne.codes@gmail.com>
2021-08-17 10:57:10 -0400

    organized new colour group page

--- client/src/pages/colour-groups/create-new-colour-group/CreateNewColourGroup.js
+++ client/src/pages/colour-groups/create-new-colour-group/CreateNewColourGroup.js
@@ -17,127 +17,129 @@ export function CreateNewColourGroup() {
     }).then((res) => history.push(`/`));
   };
 
-  const NewColourGroupForm = () => {
-    return (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          submitForm(newColourGroup);
-        }}
-      >
-        <label>Name</label>
-        <input
-          type="text"
-          onChange={(e) =>
-            setNewColourGroup({ ...newColourGroup, name: e.target.value })
-          }
-        ></input>
-
-        <NewColourGroup />
-
-        <button>Submit new colour group</button>
-      </form>
-    );
-  };
-
-  const NewColourGroup = () => {
-    return (
-      <div className="new-colour-group">
-        <h2>Colour Group</h2>
-        <div className="colour-group-colour-swatches">
-          {newColourGroup.colours.map((colour, index) => {
-            return (
-              <div
-                className="colour-group-colour-swatch"
-                key={index}
-                style={{
-                  backgroundColor: `hsl(${colour.hue}, ${colour.saturation}%, ${colour.lightness}%)`,
-                }}
-              ></div>
-            );
-          })}
-        </div>
-      </div>
-    );
-  };
-
-  const NewColourForm = () => {
-    const [newColour, setNewColour] = useState({});
-
-    return (
-      <form
-        className="new-colour-form"
-        onSubmit={(e) => {
-          e.preventDefault();
-          const updatedNewColourGroupColours = [...newColourGroup.colours];
-          updatedNewColourGroupColours.push(newColour);
-          setNewColourGroup({
-            ...newColourGroup,
-            colours: updatedNewColourGroupColours,
-          });
-          clearInputValues();
-        }}
-      >
-        <div className="label-and-input">
-          <label>hue</label>
-          <input
-            type="number"
-            className="new-colour-input"
-            onChange={(e) =>
-              setNewColour({ ...newColour, hue: e.target.value })
-            }
-          ></input>
-        </div>
-
-        <div className="label-and-input">
-          <label>saturation</label>
-          <input
-            type="number"
-            className="new-colour-input"
-            onChange={(e) =>
-              setNewColour({ ...newColour, saturation: e.target.value })
-            }
-          ></input>
-        </div>
-
-        <div className="label-and-input">
-          <label>lightness</label>
-          <input
-            type="number"
-            className="new-colour-input"
-            onChange={(e) =>
-              setNewColour({ ...newColour, lightness: e.target.value })
-            }
-          ></input>
-        </div>
-
-        <div className="new-colour-preview-div">
-          <p>Preview</p>
-          <div
-            className="new-colour-preview"
-            style={{
-              backgroundColor: `hsl(${newColour.hue}, ${newColour.saturation}%, ${newColour.lightness}%)`,
-            }}
-          ></div>
-        </div>
-
-        <button>Add new colour to group</button>
-      </form>
-    );
-  };
-
   return (
     <div className="new-colour-group-form-page">
       <div className="new-colour-group-form-div">
         <h1>New Colour Group Form</h1>
-        <NewColourGroupForm />
+        <NewColourGroupForm newColourGroup={newColourGroup} setNewColourGroup={setNewColourGroup} submitForm={submitForm}/>
       </div>
 
       <div className="new-colour-form-div">
         <h2>New Colour Form</h2>
-        <NewColourForm />
+        <NewColourForm newColourGroup={newColourGroup} setNewColourGroup={setNewColourGroup}/>
       </div>
     </div>
+  );
+}
+// ----------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------
+function NewColourGroupForm({newColourGroup, setNewColourGroup, submitForm}){
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        submitForm(newColourGroup);
+      }}
+    >
+      <label>Name</label>
+      <input
+        type="text"
+        onChange={(e) =>
+          setNewColourGroup({ ...newColourGroup, name: e.target.value })
+        }
+      ></input>
+
+      <NewColourGroup newColourGroup={newColourGroup}/>
+
+      <button>Submit new colour group</button>
+    </form>
+  );
+}
+
+function NewColourGroup({newColourGroup}){
+  return (
+    <div className="new-colour-group">
+      <h2>Colour Group</h2>
+      <div className="colour-group-colour-swatches">
+        {newColourGroup.colours.map((colour, index) => {
+          return (
+            <div
+              className="colour-group-colour-swatch"
+              key={index}
+              style={{
+                backgroundColor: `hsl(${colour.hue}, ${colour.saturation}%, ${colour.lightness}%)`,
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function NewColourForm({newColourGroup, setNewColourGroup}){
+  const [newColour, setNewColour] = useState({});
+
+  return (
+    <form
+      className="new-colour-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const updatedNewColourGroupColours = [...newColourGroup.colours];
+        updatedNewColourGroupColours.push(newColour);
+        setNewColourGroup({
+          ...newColourGroup,
+          colours: updatedNewColourGroupColours,
+        });
+        clearInputValues();
+      }}
+    >
+      <div className="label-and-input">
+        <label>hue</label>
+        <input
+          type="number"
+          className="new-colour-input"
+          onChange={(e) =>
+            setNewColour({ ...newColour, hue: e.target.value })
+          }
+        ></input>
+      </div>
+
+      <div className="label-and-input">
+        <label>saturation</label>
+        <input
+          type="number"
+          className="new-colour-input"
+          onChange={(e) =>
+            setNewColour({ ...newColour, saturation: e.target.value })
+          }
+        ></input>
+      </div>
+
+      <div className="label-and-input">
+        <label>lightness</label>
+        <input
+          type="number"
+          className="new-colour-input"
+          onChange={(e) =>
+            setNewColour({ ...newColour, lightness: e.target.value })
+          }
+        ></input>
+      </div>
+
+      <div className="new-colour-preview-div">
+        <p>Preview</p>
+        <div
+          className="new-colour-preview"
+          style={{
+            backgroundColor: `hsl(${newColour.hue}, ${newColour.saturation}%, ${newColour.lightness}%)`,
+          }}
+        ></div>
+      </div>
+
+      <button>Add new colour to group</button>
+    </form>
   );
 }
 
